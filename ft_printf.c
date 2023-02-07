b7d90fa6427bce1f24eb40007090d7315c46aa83
myne <jsamin3@gmail.com>
2023-02-07 02:19:51 +0100

    modified

--- ft_printf.c
+++ ft_printf.c
@@ -6,40 +6,40 @@
 /*   By: aerrahim <aerrahim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 16:43:26 by aerrahim          #+#    #+#             */
-/*   Updated: 2023/02/05 16:48:09 by aerrahim         ###   ########.fr       */
+/*   Updated: 2023/02/07 02:16:12 by aerrahim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_flag(va_list argp, char flag)
+int	ft_flag(va_list args, char flag)
 {
 	int	c;
 
 	c = 0;
 	if (flag == 'c')
-		c += ft_putchar(va_arg(argp, int));
+		c += ft_putchar(va_arg(args, int));
 	else if (flag == 's')
-		c += ft_putstr(va_arg(argp, char *));
+		c += ft_putstr(va_arg(args, char *));
 	else if (flag == 'd' || flag == 'i')
-		c += ft_putnbr(va_arg(argp, int));
+		c += ft_putnbr(va_arg(args, int));
 	else if (flag == 'u')
-		c += ft_putunsg(va_arg(argp, unsigned int));
+		c += ft_putunsg(va_arg(args, unsigned int));
 	else if (flag == 'x')
-		c += ft_puthexlower(va_arg(argp, unsigned int));
+		c += ft_puthexlower(va_arg(args, unsigned int));
 	else if (flag == 'X')
-		c += ft_puthexupper(va_arg(argp, unsigned int));
+		c += ft_puthexupper(va_arg(args, unsigned int));
 	else if (flag == 'p')
 	{
 		c += ft_putstr("0x");
-		c += ft_putadr(va_arg(argp, unsigned long));
+		c += ft_putadr(va_arg(args, unsigned long));
 	}
 	return (c);
 }
 
 int	ft_printf(const char *str, ...)
 {
-	va_list	argp;
+	va_list	args;
 	int		i;
 	int		x;
 
@@ -47,13 +47,13 @@ int	ft_printf(const char *str, ...)
 	i = 0;
 	if (!str)
 		return (0);
-	va_start(argp, str);
+	va_start(args, str);
 	while (str[i])
 	{
 		if (str[i] == '%')
 		{
 			if (str[++i] != '%')
-				x += ft_flag(argp, str[i]);
+				x += ft_flag(args, str[i]);
 			else
 				x += ft_putchar(str[i]);
 		}
@@ -61,6 +61,6 @@ int	ft_printf(const char *str, ...)
 			x += ft_putchar(str[i]);
 		i++;
 	}
-	va_end(argp);
+	va_end(args);
 	return (x);
 }
